936c4b9a6bf221d9861c4d932479fc7c5783f488
GD-HCK <giulio.dalix@outlook.com>
2021-07-21 12:31:57 +0100

    Added Wordpress

--- Automation/Wordpress Container Automation/WordpressBackup.ps1
+++ Automation/Wordpress Container Automation/WordpressBackup.ps1
@@ -8,12 +8,12 @@ param (
     [Parameter(Mandatory = $true)]
     [string]
     [ValidateNotNullOrEmpty()]
-    $OctopusDBContainerName,
+    $WordpressDBContainerName,
 
     [Parameter(Mandatory = $true)]
     [string]
     [ValidateNotNullOrEmpty()]
-    $OctopusWEBContainerName
+    $WordpressWEBContainerName
 )
 $dateTime = Get-Date -Format "dd-MM-yyyy_hh-mm-ss"
 
@@ -26,8 +26,8 @@ if (!$backupfolderlocation) {
 # Stop containers
 Write-Host "Stopping containers" -ForegroundColor Yellow
 
-docker container stop $OctopusDBContainerName
-docker container stop $OctopusWEBContainerName
+docker container stop $WordpressDBContainerName
+docker container stop $WordpressWEBContainerName
 
 Write-Host "Containers stopped" -ForegroundColor Green
 Start-Sleep 10
@@ -35,28 +35,25 @@ Start-Sleep 10
 Write-Host ""
 Write-Host "Backing up database files" -ForegroundColor Yellow
 $mountpoint = $backupfolder + ":/backup"
-$command = "cd /var/opt/mssql/data && tar cvf /backup/octopus_dbs_" + $dateTime + ".tar ."
-docker run --rm --volumes-from $OctopusDBContainerName -v $mountpoint ubuntu bash -c $command
+$command = "cd /var/lib/mysql && tar cvf /backup/wordpress_dbs_" + $dateTime + ".tar ."
+docker run --rm --volumes-from $WordpressDBContainerName -v $mountpoint ubuntu bash -c $command
 
 Write-Host ""
 Write-Host "Backing up web filesystem files" -ForegroundColor Yellow
-$directories = @("repository", "artifacts", "taskLogs", "cache", "import", "Octopus")
 $mountpoint = $backupfolder + ":/backup"
-foreach ($directory in $directories){
-    $command = "cd /"+$directory+" && tar cvf /backup/"+$directory+"_"+ $dateTime + ".tar ."
-    docker run --rm --volumes-from $octopusWebServer -v $mountpoint ubuntu bash -c $command
-}
+$command = "cd /var/www/html && tar cvf /backup/wordpress_web_"+ $dateTime + ".tar ."
+docker run --rm --volumes-from $WordpressWebServer -v $mountpoint ubuntu bash -c $command
 
 Write-Host ""
 Write-Host "Backup completed." -ForegroundColor Green
 
 Write-Host ""
 Write-Host "Starting DB Container" -ForegroundColor Yellow
-docker container start $OctopusDBContainerName
+docker container start $WordpressDBContainerName
 Write-Host "Waiting for DB containers to start" -ForegroundColor Yellow
 Start-Sleep 15
 Write-Host ""
 Write-Host "Starting Web Container" -ForegroundColor Yellow
-docker container start $OctopusWEBContainerName
+docker container start $WordpressWEBContainerName
 Write-Host ""
 Write-Host "Containers started. Check docker for issues" -ForegroundColor Green
---
+++ Automation/Wordpress Container Automation/WordpressRestore.ps1
@@ -0,0 +1,82 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $true)]
+    [string]
+    [ValidateNotNullOrEmpty()]
+    $backupfolder,
+
+    [Parameter(Mandatory = $true)]
+    [string]
+    [ValidateNotNullOrEmpty()]
+    $WordpressDBContainerName,
+
+    [Parameter(Mandatory = $true)]
+    [string]
+    [ValidateNotNullOrEmpty()]
+    $WordpressWEBContainerName
+)
+
+function removeBackups {
+    $removeBackupFiles = Read-Host -Prompt "Do you want to remove the backup files? yes/no"
+    if($removeBackupFiles -ieq "yes" -or $removeBackupFiles -ieq "y"){
+        $items = Get-ChildItem -Path $backupfolder
+        $items | Remove-Item
+        Write-Host "Backups removed successfully" -ForegroundColor Green
+    }elseif($removeBackupFiles -ine "no" -or $removeBackupFiles -ine "n" -or $null -ne $removeBackupFiles){
+        Write-Warning "Invalid input. Answer either yes or no (y and n are also allowed)"
+        removeBackups
+    }
+}
+
+$backupfolderlocation = Test-Path -Path $backupfolder
+
+if (!$backupfolderlocation) {
+    Write-Error "Backup folder does not exist. Aborting restore"
+    exit
+}
+else {
+# Stop containers
+Write-Host "Stopping containers" -ForegroundColor Yellow
+
+docker container stop $WordpressDBContainerName
+docker container stop $WordpressWEBContainerName
+
+Write-Host "Containers stopped" -ForegroundColor Green
+Start-Sleep 10
+
+    Write-Host ""
+    
+    $items = Get-ChildItem -Path $backupfolder
+    $mountpoint = $backupfolder+":/backup"
+    foreach ($item in $items){
+        Write-Host ""
+        Write-Host "I am working on" $item.Name -ForegroundColor Cyan
+        if(($item.Name).Contains("db")){
+            $command = "rm -rf /var/lib/mysql/* && cd /var/lib/mysql && tar xvf /backup/"+$item.Name+" ."
+            $task = "docker run --rm --volumes-from $WordpressDBContainerName -v $mountpoint ubuntu bash -c $command"
+            Write-Progress -Activity "Restoring db files" -Status "Executing command" -CurrentOperation $task
+            docker run --rm --volumes-from $WordpressDBContainerName -v $mountpoint ubuntu bash -c $command | Out-Null
+        }else{
+            $command = "rm -rf /var/www/html/* && cd /var/www/html && tar xvf /backup/"+$item.Name+" ."
+            $task = "docker run --rm --volumes-from $WordpressWEBContainerName -v $mountpoint ubuntu bash -c $command"
+            Write-Progress -Activity "Restoring Web filesystem files" -Status "Executing command" -CurrentOperation $task
+            docker run --rm --volumes-from $WordpressWEBContainerName -v $mountpoint ubuntu bash -c $command | Out-Null
+        }
+        
+    }
+    
+    Write-Host ""
+    Write-Host "Restore completed." -ForegroundColor Green
+
+    Write-Host ""
+    Write-Host "Starting DB Container" -ForegroundColor Yellow
+    docker container start $WordpressDBContainerName
+    Write-Host "Waiting for DB containers to initialise" -ForegroundColor Yellow
+    Start-Sleep 15
+    Write-Host "" 
+    Write-Host "Starting Web Container" -ForegroundColor Yellow
+    docker container start $WordpressWEBContainerName
+    Write-Host ""
+    Write-Host "Containers started. Check Docker for issues" -ForegroundColor Green
+    removeBackups
+}
